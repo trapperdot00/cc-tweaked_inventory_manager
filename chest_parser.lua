71dda3c9f1c6f989d6f14659bd24ed146c5ced76
trapperdot00 <csernusricsike@gmail.com>
2025-12-08 20:19:07 +0100

    Added file-parsing for previously serialized chest contents and item counting for chest_parser module.

--- chest_parser.lua
+++ chest_parser.lua
@@ -10,11 +10,30 @@ function chest_parser.get_chest_contents()
 	return contents
 end
 
+function chest_parser.read_from_file(filename)
+	local file = io.open(filename)
+	local contents = file:read("a")
+	file:close()
+	return textutils.unserialize(contents)
+end
+
 function chest_parser.write_to_file(contents, filename)
     local file = io.open(filename, "w")
     local serialized = textutils.serialize(contents)
     file:write(serialized)
     file:close()
+end
+
+function chest_parser.item_count(contents, sought_item)
+	local count = 0
+	for chest_name, content in pairs(contents) do
+		for slot, item in pairs(content) do
+			if item.name == sought_item then
+				count = count + item.count
+			end
+		end
+	end
+	return count
 end
 
 return chest_parser
