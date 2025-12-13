3f05d79f2060be50bb0e6444e15cc23ca2c62608
Csernus Richárd Benjámin <csernusricsike@gmail.com>
2025-12-13 03:06:16 +0100

    Removed file_utils module as there is an fs module that provides checking for existing files.

--- file_utils.lua
+++
@@ -1,8 +0,0 @@
-local file_utils = {}
-
-function file_utils.exists(filename)
-    local file = io.open(filename)
-    return file
-end
-
-return file_utils
--- main.lua
+++ main.lua
@@ -2,7 +2,6 @@ local cfg       = require("config_reader")
 local cliargs   = require("options")
 local work      = require("work_delegator")
 local Inventory = require("Inventory")
-local files		= require("file_utils")
 
 local function main()
     -- Update this to the current working directory:
@@ -13,20 +12,20 @@ local function main()
     local inventory_file = pwd .. "items.data"
     local inputs_file    = pwd .. "inputs.txt"
 
-	if not files.exists(inputs_file) then
-		print("Input-chest file '" .. inputs_file .. "' doesn't exist.")
-		return
-	end
+    if not fs.exists(inputs_file) then
+        print("Input-chest file '" .. inputs_file .. "' doesn't exist.")
+        return
+    end
 
     local options   = cliargs.parse()
     local inputs    = cfg.read_seque(inputs_file)
     local inventory = Inventory.new(inputs, inventory_file)
     
     -- Select appropriate work for command-line arguments
-	local t1 = os.clock()
+    local t1 = os.clock()
     work.delegate(options, inventory)
-	local t2 = os.clock()
-	print("elapsed time:", t2 - t1, "seconds.")
+    local t2 = os.clock()
+    print("elapsed time:", t2 - t1, "seconds.")
 end
 
 main()
