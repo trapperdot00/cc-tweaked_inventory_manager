e0eb4e86c3ed41ae4119f43c194a259f53815026
Csernus Richárd Benjámin <csernusricsike@gmail.com>
2025-12-13 03:43:43 +0100

    Added more sophisticated command-line argument handling.

--- options.lua
+++ options.lua
@@ -1,34 +1,52 @@
 local str     = require("utils.string_utils")
 local options = {}
+options.__index = options
 
 function options.parse()
-    local opts = {
+    local self = {
+        -- Exclusive arguments:
         push            = false,
         pull            = false,
-        scan            = false,
         get             = {},
         count           = {},
+        -- Non-exclusive:
+        scan            = false,
         print_inputs    = false
     }
+    setmetatable(self, options)
     for i = 1, #arg do
         local curr_arg = arg[i]
         if curr_arg == "--push" then
-            opts.push = true
-        elseif curr_arg == "--pull" then
-            opts.pull = true
-        elseif curr_arg == "--scan" then
-            opts.scan = true
-        elseif curr_arg:find("--get", 1, true) then
+            self.push = true
+        end
+        if curr_arg == "--pull" then
+            self.pull = true
+        end
+        if curr_arg == "--scan" then
+            self.scan = true
+        end
+        if curr_arg:find("--get", 1, true) then
             local equal_pos = curr_arg:find('=', 1, true) + 1
-            opts.get = str.split(curr_arg:sub(equal_pos), ',')
-        elseif curr_arg:find("--count", 1, true) then
+            self.get = str.split(curr_arg:sub(equal_pos), ',')
+        end
+        if curr_arg:find("--count", 1, true) then
             local equal_pos = curr_arg:find('=', 1, true) + 1
-            opts.count = str.split(curr_arg:sub(equal_pos), ',')
-        elseif curr_arg == "--print-inputs" then
-            opts.print_inputs = true
+            self.count = str.split(curr_arg:sub(equal_pos), ',')
+        end
+        if curr_arg == "--print-inputs" then
+            self.print_inputs = true
         end
     end
-    return opts
+    return self
+end
+
+function options:valid()
+    local excl_cnt = 0
+    if self.push then excl_cnt = excl_cnt + 1 end
+    if self.pull then excl_cnt = excl_cnt + 1 end
+    if #self.get   > 0 then excl_cnt = excl_cnt + 1 end
+    if #self.count > 0 then excl_cnt = excl_cnt + 1 end
+    return excl_cnt <= 1
 end
 
 return options
--- work_delegator.lua
+++ work_delegator.lua
@@ -14,21 +14,24 @@ local function print_help()
 end
 
 function work_delegator.delegate(options, inventory)
+    if #arg == 0 or not options:valid() then
+        print_help()
+        return
+    end
+	if options.scan then
+		inventory:scan()
+	elseif options.print_inputs then
+		print_inputs(inventory)
+	end
 	if options.push then
 		inventory:push()
 	elseif options.pull then
 		inventory:pull()
-	elseif options.scan then
-		inventory:scan()
 	elseif #options.get > 0 then
 		inventory:get(options.get)
 	elseif #options.count > 0 then
 		inventory:count(options.count)
-	elseif options.print_inputs then
-		print_inputs(inventory)
-	else
-		print_help()
-	end
+    end
 end
 
 return work_delegator
