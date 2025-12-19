0829b2ed907630db63a6be4e09f52bc366935834
Csernus Richárd Benjámin <csernusricsike@gmail.com>
2025-12-19 20:24:13 +0100

    Program now creates a data directory for the content, input, and stack files.

--- main.lua
+++ main.lua
@@ -1,15 +1,32 @@
 local cliargs   = require("options")
 local work      = require("work_delegator")
 
+local function create_directory(dir)
+    if not fs.exists(dir) then
+        fs.makeDir(dir)
+    elseif not fs.isDir(dir) then
+        return false
+    end
+    return true
+end
+
 local function main()
     -- Get current working directory
     -- based on the command's directory specification
-    local pwd = fs.getDir(shell.resolve(arg[0]))
+    local pwd  = fs.getDir(shell.resolve(arg[0]))
 
-    -- Files
-    local contents_path = fs.combine(pwd, "contents.data")
-    local inputs_path   = fs.combine(pwd, "inputs.data")
-    local stacks_path   = fs.combine(pwd, "stacks.data")
+    -- Create data directory
+    local data_dir = fs.combine(pwd, "data")
+    if not create_directory(data_dir) then
+        printError("Cannot create '"..data_dir.."' directory.")
+        print("Try deleting the file that shares the same path.")
+        return
+    end
+
+    -- Data file paths
+    local contents_path = fs.combine(data_dir, "contents.data")
+    local inputs_path   = fs.combine(data_dir, "inputs.data")
+    local stacks_path   = fs.combine(data_dir, "stacks.data")
 
     local options = cliargs.parse()
     
