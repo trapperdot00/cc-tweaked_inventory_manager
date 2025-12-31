bdb0572cb8f89ff5481f64012f4c242b078b169a
trapperdot00 <csernusricsike@gmail.com>
2025-12-31 21:22:42 +0100

    Added inventory database class 'inv_db' to abstract table manipulations behind a convenient interface.

---
+++ src/inv_db.lua
@@ -0,0 +1,146 @@
+local tbl = require("utils.table_utils")
+
+local inv_db = {}
+inv_db.__index = inv_db
+
+--==== INTERFACE ====--
+
+function inv_db.new() end
+
+-- Inventories
+function inv_db:inv_exists(inv_id) end
+function inv_db:add_inv(inv_id, size) end
+function inv_db:del_inv(inv_id) end
+function inv_db:get_inv_ids() end
+function inv_db:get_size(inv_id) end
+
+-- Items
+function inv_db:item_exists(inv_id, slot) end
+function inv_db:add_item(inv_id, slot, item) end
+function inv_db:del_item(inv_id, slot) end
+function inv_db:get_item(inv_id, slot) end
+function inv_db:get_items(inv_id) end
+
+--==== IMPLEMENTATION ====--
+
+function inv_db.new()
+    local self = setmetatable(
+        {
+            data = {}
+        }, inv_db
+    )
+    return self
+end
+
+function inv_db:inv_exists(inv_id)
+    return self.data[inv_id] ~= nil
+end
+
+local function
+throw_if_inv_exists(self, inv_id)
+    if self:inv_exists(inv_id) then
+        error("inventory " ..
+            inv_id .. " already exists", 0)
+    end
+end
+
+local function
+throw_if_inv_doesnt_exist(self, inv_id)
+    if not self:inv_exists(inv_id) then
+        error("inventory " ..
+            inv_id .. " doesn't exist", 0)
+    end
+end
+
+function inv_db:add_inv(inv_id, size)
+    self.data[inv_id] = {
+        size  = size,
+        items = {}
+    }
+end
+
+function inv_db:del_inv(inv_id)
+    throw_if_inv_doesnt_exist(self, inv_id)
+    self.data[inv_id] = nil
+end
+
+function inv_db:get_inv_ids()
+    return tbl.get_keys(self.data)
+end
+
+function inv_db:get_size(inv_id)
+    throw_if_inv_doesnt_exist(self, inv_id)
+    return self.data[inv_id].size
+end
+
+local function
+throw_if_slot_out_of_range(self, inv_id, slot)
+    local inv_size = self:get_size(inv_id)
+    if slot < 1 or slot > inv_size then
+        error(
+            "slot out of range for " ..
+            "inventory '" .. inv_id ..
+            "' (size: " ..
+            tostring(inv_size) ..
+            ", got: " ..
+            tostring(slot) .. ")"
+            , 0
+        )
+    end
+end
+
+function inv_db:item_exists(inv_id, slot)
+    throw_if_inv_doesnt_exist(self, inv_id)
+    return self.data[inv_id].items[slot] ~= nil
+end
+
+local function
+throw_if_item_exists(self, inv_id, slot)
+    if self:item_exists(inv_id, slot) then
+        error("item already exists " ..
+            "in inventory '" .. inv_id ..
+            "' at slot " .. tostring(slot), 0)
+    end
+end
+
+local function
+throw_if_item_doesnt_exist(self, inv_id, slot)
+    if not self:item_exists(inv_id, slot) then
+        error("item doesn't exist " ..
+            "in inventory '" .. inv_id ..
+            "' at slot " .. tostring(slot), 0)
+    end
+end
+
+function inv_db:add_item(inv_id, slot, item)
+    throw_if_slot_out_of_range(
+        self, inv_id, slot
+    )
+    self.data[inv_id].items[slot] = item
+end
+
+function inv_db:del_item(inv_id, slot)
+    throw_if_item_doesnt_exist(
+        self, inv_id, slot
+    )
+    self.data[inv_id].items[slot] = nil
+end
+
+function inv_db:get_item(inv_id, slot)
+    throw_if_slot_out_of_range(
+        self, inv_id, slot
+    )
+    throw_if_item_doesnt_exist(
+        self, inv_id, slot
+    )
+    return self.data[inv_id].items[slot]
+end
+
+function inv_db:get_items(inv_id)
+    throw_if_inv_doesnt_exist(
+        self, inv_id
+    )
+    return self.data[inv_id].items
+end
+
+return inv_db
